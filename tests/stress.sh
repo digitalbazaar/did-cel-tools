e89a439ea74e514aad4909aeacf12626b17735eb
Manu Sporny <msporny@digitalbazaar.com>
2025-12-02 14:35:45 -0500

    Add stress test for 10 year DID w/ 3 month key cycle times.

---
+++ tests/stress.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Create a large DID Document and compress it
+
+../didcel -c create -c witness \
+         -c "add authentication ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "add assertionMethod ecdsa" -c update -c witness \
+         -c "ls" -c save -c quit
